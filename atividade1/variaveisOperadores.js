d4584b2a0c4fa59352c6e65246f12c691585133c
Renan Costa <renancs.sousa@gmail.com>
2024-03-16 10:49:04 -0300

    Atividade1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,11 @@
+var n1 = 63;
+var n2 = 35;
+var soma = n1 + n2;
+var subtracao = n1 - n2;
+var multiplicacao = n1 * n2;
+var divisao = n1 / n2;
+
+console.log(`Resultado da adição entre ${n1} e ${n2}: ${soma}`);
+console.log(`Resultado da subtração entre ${n1} e ${n2}: ${subtracao}`);
+console.log(`Resultado da multiplicação entre ${n1} e ${n2}: ${multiplicacao}`);
+console.log(`Resultado da divisão entre ${n1} e ${n2}: ${divisao}`);
